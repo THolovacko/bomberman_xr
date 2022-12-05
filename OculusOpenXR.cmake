67c8581c7c375698312f5daf5728f634005df51b
Ryan Pavlik <ryan.pavlik@collabora.com>
2022-12-05 12:23:35 -0600

    more adjust for moving

--- OculusOpenXR.cmake
+++ OculusOpenXR.cmake
@@ -1,14 +1,11 @@
 
 add_library(OpenXR::openxr_loader SHARED IMPORTED)
 target_include_directories(OpenXR::openxr_loader INTERFACE
-  ${solution_dir}dependencies/ovr_openxr_mobile_sdk_42.0/3rdParty/khronos/openxr/OpenXR-SDK/include/)
+  dependencies/ovr_openxr_mobile_sdk_42.0/3rdParty/khronos/openxr/OpenXR-SDK/include/)
 
-if (CMAKE_BUILD_TYPE STREQUAL "Debug")
-  set_target_properties(OpenXR::openxr_loader PROPERTIES IMPORTED_LOCATION
-    ${CMAKE_CURRENT_SOURCE_DIR}${solution_dir}../dependencies/ovr_openxr_mobile_sdk_42.0/OpenXR/Libs/Android/arm64-v8a/Debug/libopenxr_loader.so
-  )
-else()
-  set_target_properties(OpenXR::openxr_loader PROPERTIES IMPORTED_LOCATION
-    ${CMAKE_CURRENT_SOURCE_DIR}${solution_dir}../dependencies/ovr_openxr_mobile_sdk_42.0/OpenXR/Libs/Android/arm64-v8a/Release/libopenxr_loader.so
-  )
-endif()
+set_target_properties(OpenXR::openxr_loader PROPERTIES IMPORTED_LOCATION_DEBUG
+  ${CMAKE_CURRENT_SOURCE_DIR}/dependencies/ovr_openxr_mobile_sdk_42.0/OpenXR/Libs/Android/arm64-v8a/Debug/libopenxr_loader.so
+)
+set_target_properties(OpenXR::openxr_loader PROPERTIES IMPORTED_LOCATION
+  ${CMAKE_CURRENT_SOURCE_DIR}/dependencies/ovr_openxr_mobile_sdk_42.0/OpenXR/Libs/Android/arm64-v8a/Release/libopenxr_loader.so
+)
